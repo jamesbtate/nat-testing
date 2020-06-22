b0ccb753c88ffa5739f542a0d62c398cad07ca1e
James Tate II <jtate@odu.edu>
2020-06-22 13:27:00 -0400

    add --sweep argument and --port argument

--- udp.py
+++ udp.py
@@ -9,7 +9,12 @@ def parse_args():
     desc = "UDP client/server script and library for NAT testing"
     parser = argparse.ArgumentParser(description=desc)
     parser.add_argument('-c', '--client', metavar="HOST", nargs='*')
+    parser.add_argument('-p', '--port', metavar="DESTPORT", type=int,
+                        default=7777)
     parser.add_argument('-s', '--server', metavar="PORT", type=int)
+    parser.add_argument('-w', '--sweep', metavar="N", type=int,
+                        help="Sweep N connections to the same destination. \
+                        Don't wait for a response.")
     args = parser.parse_args()
     if not args.client and not args.server:
         parser.error("Must specify client-mode or server-mode")
@@ -34,6 +39,14 @@ def send(host, port, message):
           _socket.getsockname()[1])
 
 
+def send_from_new_socket(host, port, message):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    host = socket.gethostbyname(host)
+    s.sendto(message.encode('ascii'), (host, port))
+    print("sent", '"'+message+'"', "to", (host, port), "from port",
+          s.getsockname()[1])
+
+
 def recv():
     """ returns (host, port, message) """
     if not _socket:
@@ -49,15 +62,21 @@ if __name__ == '__main__':
     print(args)
     try:
         if args.client:
-            make_socket()
-            for host in args.client:
-                send(host, 7777, 'test blah')
-            for i in range(len(args.client)):
-                recv()
+            n = 1
+            if args.sweep:
+                n = args.sweep
+            for i in range(n):
+                make_socket()
+                for host in args.client:
+                    send(host, args.port, 'test blah')
+                if not args.sweep:
+                    while True:
+                        recv()
         elif args.server:
             make_socket(port=args.server)
             while True:
                 host, port, message = recv()
                 send(host, port, "reply to " + message)
+                send_from_new_socket(host, port, message + " from other port")
     except KeyboardInterrupt:
         sys.exit(1)
